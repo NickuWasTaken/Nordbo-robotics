411a1d4a2607527cf20d5e94ab6ba41f074d79d5
NickuWasTaken <74908215+NickuWasTaken@users.noreply.github.com>
2023-04-04 07:30:45 +0200

    Added SelectionModal.vue
    + added header styles to base.css

---
+++ src/components/UI/SelectionModal.vue
@@ -0,0 +1,223 @@
+<script setup>
+/*
+Kald: 
+		<TheModal 
+		categoryType="" 
+		productImage="" 
+		productName="" 
+		productDetails=""
+		/> 
+*/
+
+import { ref } from 'vue';
+
+const objPros = [
+	{
+		id: 1,
+		desc: 'Possibility to include industrial touch screen',
+	},
+	{
+		id: 2,
+		desc: 'Targeted at robot first timers (no-timers)',
+	},
+	{
+		id: 3,
+		desc: 'GUI and Mimic settings preset for sanding',
+	},
+];
+const objCons = [
+	{
+		id: 1,
+		desc: 'Need a conveyor function',
+	},
+];
+
+const props = defineProps({
+	categoryType: {
+		type: String,
+		default: 'sanding',
+	},
+	productImage: {
+		type: String,
+		default: 'Sander-1',
+	},
+	productName: {
+		type: String,
+		default: 'Mimic urcap',
+	},
+	productDetails: {
+		type: String,
+		default:
+			'This robot is designed to handle complex sanding projects with precision and efficiency. With its advanced features and customizable programming options, this sanding robot is ideal for manufacturers and craftsmen looking to streamline their production processes.',
+	},
+});
+</script>
+
+<template>
+	<div class="wrap">
+		<section class="modal">
+			<aside class="modal__product">
+				<img :src="'./src/assets/images/' + props.productImage + '.png'" />
+			</aside>
+			<main class="modal__info">
+				<header class="modal__info__header">
+					<p class="modal__info__header__close">
+						<img
+							src="@/assets/icons/close-icon.png"
+							class="modal__info__header__close__icon"
+						/>
+						Close
+					</p>
+				</header>
+				<div class="modal__info_product">
+					<caption>
+						{{
+							props.categoryType
+						}}
+					</caption>
+					<h1>{{ props.productName }}</h1>
+					<p>{{ props.productDetails }}</p>
+				</div>
+
+				<ul class="modal__info__pros">
+					<h2>Pros</h2>
+					<li v-for="pro in objPros" :key="pro">
+						<div class="pro-list-icon"></div>
+						<p>{{ pro.desc }}</p>
+					</li>
+				</ul>
+				<h2>Cons</h2>
+				<ul class="modal__info__cons">
+					<li v-for="con in objCons" :key="con">
+						<div class="con-list-icon"></div>
+						<p>{{ con.desc }}</p>
+					</li>
+				</ul>
+				<a class="modal__info__button">Select</a>
+			</main>
+		</section>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.wrap {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 1280px;
+
+	.modal {
+		height: 900px;
+		border-radius: 15px;
+		background: #fff;
+		border: var(--color-background-soft) solid 1px;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		box-shadow: 2px 6px 37px -6px rgba(0, 0, 0, 0.08);
+
+		&__product {
+			padding: 64px;
+			background: linear-gradient(
+				180deg,
+				rgba(98, 195, 233, 0.5) 0%,
+				rgba(98, 195, 233, 0) 100%
+			);
+			border-radius: 14px 0 0 14px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+
+		&__info {
+			padding: 64px;
+			padding-top: 0;
+
+			&__header {
+				height: 88px;
+				margin-bottom: 40px;
+				border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+				display: flex;
+				align-items: center;
+				justify-content: end;
+
+				&__close {
+					color: #000;
+					display: flex;
+					align-items: center;
+					cursor: pointer;
+					padding: 16px 0px 16px 32px;
+					margin: 0;
+
+					&__icon {
+						padding-right: 10px;
+					}
+				}
+			}
+
+			h1,
+			h2 {
+				margin: 0;
+				text-transform: uppercase;
+			}
+
+			caption {
+				color: var(--inactive-default);
+			}
+
+			&__button {
+				background: var(--color-button-brand-default);
+				text-align: center;
+				padding: 16px 0;
+				color: #000;
+				border-radius: 56px;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				cursor: pointer;
+			}
+
+			&__button:hover {
+				background: var(--color-button-brand-hover);
+				color: var(--brand-font-active);
+			}
+
+			&__pros,
+			&__cons {
+				padding-left: 0;
+				list-style: none;
+				margin-top: 16px;
+				margin-bottom: 40px;
+
+				h2 {
+					margin-top: 40px;
+					margin-bottom: 16px;
+				}
+
+				li {
+					padding-left: 5px;
+					display: flex;
+					align-items: center;
+				}
+
+				li p {
+					display: inline-block;
+					margin: 0;
+				}
+			}
+		}
+	}
+}
+
+.pro-list-icon,
+.con-list-icon {
+	display: inline-block;
+	height: 24px;
+	width: 20px;
+	margin-right: 10px;
+	background: url('@/assets/icons/pro-icon.png') center no-repeat;
+}
+
+.con-list-icon {
+	background: url('@/assets/icons/con-icon.png') center no-repeat;
+}
+</style>
